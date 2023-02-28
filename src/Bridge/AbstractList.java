72c43e9192c451570c22f0b40e167a8185c4660b
pupureum <pureuml.dev@gmail.com>
2023-02-28 23:20:48 +0900

    feat: Bridge pattern

---
+++ src/Bridge/AbstractList.java
@@ -0,0 +1,14 @@
+package Bridge;
+
+public interface AbstractList<T> {
+
+    public void addElement(T obj);
+
+    public T deleteElement(int i);
+
+    public int insertElement(T obj, int i);
+
+    public T getElement(int i);
+
+    public int getElementSize();
+}
---
+++ src/Bridge/ArrayImpl.java
@@ -0,0 +1,36 @@
+package Bridge;
+
+import java.util.ArrayList;
+
+public class ArrayImpl<T> implements AbstractList<T>{
+    ArrayList<T> arrayList;
+
+    public ArrayImpl() {
+        arrayList = new ArrayList<T>();
+    }
+    @Override
+    public void addElement(T obj) {
+        arrayList.add(obj);
+    }
+
+    @Override
+    public T deleteElement(int i) {
+        return arrayList.remove(i);
+    }
+
+    @Override
+    public int insertElement(T obj, int i) {
+        arrayList.add(i, obj);
+        return i;
+    }
+
+    @Override
+    public T getElement(int i) {
+        return arrayList.get(i);
+    }
+
+    @Override
+    public int getElementSize() {
+        return arrayList.size();
+    }
+}
